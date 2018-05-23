c98f31c3e1aa12bf96900faa4219e119298c13ed
Billy.Zheng <zw963@163.com>
2018-05-23 12:03:42 +0800

    use string key instead of symbol key.

--- lib/sequel_enum.rb
+++ lib/sequel_enum.rb
@@ -5,29 +5,28 @@ module Sequel
       end
 
       module ClassMethods
-        
+
         def enums
           @enums ||= {}
         end
 
         def enum(column, values)
           if values.is_a? Hash
-            values.each do |key,val|
-              raise ArgumentError, "index should be a symbol, #{key} provided which it's a #{key.class}" unless key.is_a? Symbol
-              raise ArgumentError, "value should be an integer, #{val} provided which it's a #{val.class}" unless val.is_a? Integer
+            values.each do |key, val|
+              fail ArgumentError, "value should be an integer, #{val} provided which it's a #{val.class}" unless val.is_a? Integer
             end
           elsif values.is_a? Array
             values = Hash[values.map.with_index { |v, i| [v, i] }]
           else
-            raise ArgumentError, "#enum expects the second argument to be an array of symbols or a hash like { :symbol => integer }"
+            fail ArgumentError, "#enum expects the second argument to be an array of symbols or a hash like { :symbol => integer }"
           end
 
           define_method "#{column}=" do |value|
-            val = self.class.enums[column].assoc(value.to_sym)
+            val = self.class.enums[column].assoc(value.to_s)
             self[column] = val && val.last
           end
 
-          define_method "#{column}" do
+          define_method column do
             val = self.class.enums[column].rassoc(self[column])
             val && val.first
           end
